743e2735bce3a42658b3323df61713ebe20189b0
Groozly <elfimov@gmail.com>
2012-03-05 15:44:12 +0400

    extended os detection

--- lib/checkBrowser.php
+++ lib/checkBrowser.php
@@ -11,8 +11,15 @@ class checkBrowser {
 		'msie'		=> 8
 	);
 	
+	// не выводить предупреждение для пользователей этих ОС
+	public static $notOldOS = array(
+		'symbian', 'android', 'ios', 'blackberry', 'j2me',
+		'windows mobile', 'webos', 'palm', 'playbook',
+		'playstation', 'wii', 'xbox', 'robot'
+	);
 	
 	public static function warning($out='', $always=false) {
+		
 		if ($always || (self::cookie() && self::isOld())) { 
 		
 			if ($out=='') {
@@ -51,80 +58,124 @@ class checkBrowser {
 		$browser = self::getBrowser();
 
 		// для неизвестных браузеров не показываем предупреждение, т.к. это может быть мобильное устройство, тв-приставка и что угодно
-		if ($browser['platform']=='' || $browser['version']==0 || $browser['short']=='') { 
+		if ($browser['os']=='' || $browser['version']==0 || $browser['short']=='') { 
 			return false;
 		}
 
-		return isset(self::$modernVersion[$browser['short']]) && $browser['version']<self::$modernVersion[$browser['short']];
+		return isset(self::$modernVersion[$browser['short']]) 
+				&& $browser['version'] < self::$modernVersion[$browser['short']]
+				&& !in_array($browser['os'], self::$notOldOS);
 	}
 
 
 	public static function getBrowser() {
 	
-		$u_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+		$userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
 
 		// операционная система
-		if (preg_match('/windows|win32|win64/i', $u_agent)) {
-			$platform = 'windows';
+		if (preg_match('/windows mobile|windows phone|windows ce/i', $userAgent)) {
+			$os = 'windows mobile';
 		} else
-		if (preg_match('/macintosh|mac os x/i', $u_agent)) {
-			$platform = 'mac';
+		if (preg_match('/windows|win32|win64/i', $userAgent)) {
+			$os = 'windows';
 		} else
-		if (preg_match('/linux/i', $u_agent)) {
-			$platform = 'linux';
+		if (preg_match('/macintosh|mac os x/i', $userAgent)) {
+			$os = 'mac';
+		} else
+		if (stripos($userAgent, 'android')!==false) {
+			$os = 'android';
+		} else
+		if (preg_match('/ipad|iphone|ipod/i', $userAgent)) {
+			$os = 'ios';
+		} else
+		if (preg_match('/series 60|symbos|symbian/i', $userAgent)) {
+			$os = 'symbian';
+		} else
+		if (stripos($userAgent, 'blackberry')!==false) {
+			$os = 'blackberry';
+		} else
+		if (preg_match('/j2me|midp/i', $userAgent)) {
+			$os = 'j2me';
+		} else
+		if (stripos($userAgent, 'xbox')!==false) {
+			$os = 'xbox';
+		} else
+		if (stripos($userAgent, 'wii')!==false) {
+			$os = 'wii';
+		} else
+		if (stripos($userAgent, 'playstation')!==false) {
+			$os = 'playstation';
+		} else
+		if (stripos($userAgent, 'webos')!==false) {
+			$os = 'webos';
+		} else
+		if (stripos($userAgent, 'palmos')!==false) {
+			$os = 'palm';
+		} else
+		if (stripos($userAgent, 'playbook')!==false) {
+			$os = 'playbook';
+		} else
+		if (preg_match('/robot|spider|bot|crawl|search|w3c_validator|jigsaw|search/i', $userAgent)) {
+			$os = 'robot';
+		} else
+		if (stripos($userAgent, 'linux')!==false) {
+			$os = 'linux';
+		} else
+		if (stripos($userAgent, 'freebsd')!==false) {
+			$os = 'freebsd';
 		} else {
-			$platform = '';
+			$os = '';
 		}
 	   
 		// название браузера 
-		if (preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) {
-			$bname = 'Internet Explorer';
-			$ub = 'MSIE';
+		if (stripos($userAgent, 'msie')!==false && stripos($userAgent, 'opera')===false ) {
+			$name = 'Internet Explorer';
+			$short = 'msie';
 		} else
-		if (preg_match('/Firefox/i',$u_agent)) {
-			$bname = 'Mozilla Firefox';
-			$ub = 'Firefox';
+		if (stripos($userAgent, 'firefox')!==false) {
+			$name = 'Mozilla Firefox';
+			$short = 'firefox';
 		} else
-		if (preg_match('/Chrome/i',$u_agent)) {
-			$bname = 'Google Chrome';
-			$ub = 'Chrome';
+		if (stripos($userAgent, 'chrome')!==false) {
+			$name = 'Google Chrome';
+			$short = 'chrome';
 		} else
-		if (preg_match('/Opera/i',$u_agent)) {
-			$bname = 'Opera';
-			$ub = 'Opera';
+		if (stripos($userAgent, 'opera')!==false) {
+			$name = 'Opera';
+			$short = 'opera';
 		} else
-		if (preg_match('/Safari/i',$u_agent)) {
-			$bname = 'Apple Safari';
-			$ub = 'Safari';
+		if (stripos($userAgent, 'safari')!==false) {
+			$name = 'Apple Safari';
+			$short = 'safari';
 		} else
-		if (preg_match('/Netscape/i',$u_agent)) {
-			$bname = 'Netscape';
-			$ub = 'Netscape';
+		if (stripos($userAgent, 'netscape')!==false) {
+			$name = 'Netscape';
+			$short = 'netscape';
 		} else {
-			$bname = '';
-			$ub = '';
+			$name = '';
+			$short = '';
 		}
 
 		// пытаемся узнать номер версии, создаем паттерн для регэкспа, ищем и находим
-		if ($ub=='MSIE') { // MSIE в режиме совместимости попытается нас обмануть 
-			if (strpos($u_agent, 'Trident/4.0')!==false) {
+		if ($short=='MSIE') { // MSIE в режиме совместимости попытается нас обмануть 
+			if (strpos($userAgent, 'Trident/4.0')!==false) {
 				$version = '8.0';
 			} else 
-			if (strpos($u_agent, 'Trident/5.0')!==false) {
+			if (strpos($userAgent, 'Trident/5.0')!==false) {
 				$version = '9.0';
 			}
 			// как будет обманывать IE 10 предсказать невозможно
 		}
 		if (!isset($version)) {
-			$pattern = '#(?<browser>Version|' . $ub . '|other)[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-			if (!preg_match_all($pattern, $u_agent, $matches)) {
+			$pattern = '#(?<browser>Version|' . $short . '|other)[/ ]+(?<version>[0-9.|a-zA-Z.]*)#i';
+			if (!preg_match_all($pattern, $userAgent, $matches)) {
 				// нет совпадений. возможно, стоит как-то обрабатывать этот случай, а пока просто продолжаем
 			}
 
 			// проверяем сколько совпадений с паттерном
 			if (count($matches['browser']) > 1) {
 				//если совпадений больше 1, проверяем где стоит слово Version - до или после номера
-				if (strripos($u_agent, 'Version') < strripos($u_agent, $ub)) {
+				if (strripos($userAgent, 'Version') < strripos($userAgent, $short)) {
 					$version = $matches['version'][0];
 				} else {
 					$version = $matches['version'][1];
@@ -137,22 +188,15 @@ class checkBrowser {
 				}
 			}
 		}
-		// проверяем есть ли у нас номер версии
-		if (!isset($version) || $version=='') {
-			$version = '?';
-			$version_int = 0;
-		} else {
-			$version_int = (int)$version;
-		}
 		
-		$version = (!isset($version) || $version=='') ? 0 : (int)$version;
+		$version = (!isset($version) || $version=='') ? 0 : (int) $version;
 
 		return array(
-			'user_agent'	=> $u_agent,
-			'short'			=> strtolower($ub),
-			'name'			=> $bname,
+			'userAgent'		=> $userAgent,
+			'short'			=> $short,
+			'name'			=> $name,
 			'version'		=> $version,
-			'platform'		=> $platform,
+			'os'			=> $os,
 		);
 	}
 
